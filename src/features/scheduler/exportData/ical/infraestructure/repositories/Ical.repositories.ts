78789679ae39691d87a96ef6d65d26eafdf18faa
Íñigo Aréjula Aísa <arejula27@gmail.com>
2022-01-03 20:16:52 +0100

    funciona boton ical

--- src/features/scheduler/exportData/ical/infraestructure/repositories/Ical.repositories.ts
+++ src/features/scheduler/exportData/ical/infraestructure/repositories/Ical.repositories.ts
@@ -1,15 +1,11 @@
-import { isCallSignatureDeclaration } from "typescript";
 import { Result } from "../../../../../../core/config/result";
 import { icalData } from "../data_sources/Ical.data";
 import IcalParamsDTO from "../dto/IcalParamsDTO";
 
 export const icalRepo= {
-    getIcal:async(params:IcalParamsDTO):Promise<String>=>{
+    getIcal:async(params:IcalParamsDTO):Promise<Result<String>>=>{
         
-        const res = await icalData.getIcal(params);
-        if(res.isError){
-            return Promise.reject("Error al obtener calendario"); 
-        }
-        return Promise.resolve(res.value);
+        return await icalData.getIcal(params);
+        
     }
 }
