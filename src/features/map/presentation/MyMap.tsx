70b4399a7d6457b197d210fdc8ba579be4d83f8d
Heku <heku@pop-os.localdomain>
2022-03-10 10:38:11 +0100

    Conseguido que sean transparentes las capas WMS

--- src/features/map/presentation/MyMap.tsx
+++ src/features/map/presentation/MyMap.tsx
@@ -14,28 +14,32 @@ export const MyMap = () => {
   return (
     <MapContainer center={[41.65, -0.88]} zoom={13.2} scrollWheelZoom={true}>
       <LayersControl>
-        <BaseLayer name="OpenStreetMap">
+        <BaseLayer checked name="OpenStreetMap">
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <LayersControl.Overlay name= "marker">
-          <Marker position={[41.65, -0.88]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+          <LayersControl.Overlay name="marker">
+            <Marker position={[41.65, -0.88]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
           </LayersControl.Overlay>
-          <LayersControl.Overlay name= "ESPAÑA">
-          <WMSTileLayer
-                layers={'OI.OrthoimageCoverage'}
-                attribution='&copy; <a href="https://pnoa.ign.es/">IGN</a>'
-                url="http://www.ign.es/wms-inspire/pnoa-ma?"
-              />
+          <LayersControl.Overlay name="ESPAÑA">
+            <WMSTileLayer
+              transparent
+              format="image/png"
+              layers={"OI.OrthoimageCoverage"}
+              attribution='&copy; <a href="https://pnoa.ign.es/">IGN</a>'
+              url="http://www.ign.es/wms-inspire/pnoa-ma?"
+            />
           </LayersControl.Overlay>
         </BaseLayer>
-        <LayersControl.Overlay name= "ESPAÑA">
+        <LayersControl.Overlay name="EEUU">
           <WMSTileLayer
+            format="image/png"
+            transparent
             layers="topp:states"
             url="http://172.18.0.5:8080/geoserver/topp/wms?"
           />
