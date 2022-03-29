592b6b714e866c8a768bfcbde8e9b7fc3aba0de3
Íñigo Aréjula Aísa <arejula27@gmail.com>
2022-03-29 19:16:45 +0200

    usa cookies para tener sesión persistente

--- src/core/context/UserContext.tsx
+++ src/core/context/UserContext.tsx
@@ -1,13 +1,29 @@
 import { FC, useState } from "react";
-import User from "../../features/auth/domain/models/User";
+import User, { Privileges } from "../../features/auth/domain/models/User";
 import {  UserContext } from "./context";
 export const DefaultUser: User = {name:"", privileges:"none"}
 
 export const UserContextWrapper: FC = ({ children }) => {
-  const [usr, setUsr] = useState<User>(DefaultUser);
+
+
+  const initUsr=():User=>{
+    if(localStorage.getItem("name")===null)
+      return DefaultUser
+    
+    return {
+      name:localStorage.getItem("name")!,
+      privileges:localStorage.getItem("priv")! as Privileges,
+    }
+  }
+  const [usr, setUsr] = useState<User>(initUsr());
   const actions = {
     login: (newUsr: User) => {
+
+      
+      
       setUsr(newUsr);
+      localStorage.setItem("name",newUsr.name)
+      localStorage.setItem("priv",newUsr.privileges!)
       console.log("login")
       console.log(newUsr)
     
@@ -15,6 +31,8 @@ export const UserContextWrapper: FC = ({ children }) => {
     logout: () => {
       console.log(usr)
       setUsr(DefaultUser);
+      localStorage.setItem("name",DefaultUser.name)
+      localStorage.setItem("priv",DefaultUser.privileges!)
       console.log("logout")
       console.log(usr)
      
