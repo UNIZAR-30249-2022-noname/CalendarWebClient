51f7ce86ad04cf2fa91c9f80620340fb5d8690c5
dolansete <dolancc00@gmail.com>
2022-04-19 20:14:30 +0200

    Ya se pillar el input como string

--- src/features/data/presentation/components/ImportForm.tsx
+++ src/features/data/presentation/components/ImportForm.tsx
@@ -1,36 +1,31 @@
 import { Upload, message } from "antd";
 import { InboxOutlined } from "@ant-design/icons";
+import React, { useEffect, useState } from "react";
 import { httpServices } from "../../../../core/backend/http/services";
-
-const { Dragger } = Upload;
+import Text from "antd/lib/typography/Text";
 
 export const ImportForm = () => {
+  const [upload, setUpload] = useState<File>();
+  const [content, setContent] = useState("");
+  function onFileChange(event: any) {
+    // Update the state
+    setUpload(event.target.files[0]);
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      console.log(typeof reader.result);
+      console.log(reader.result);
+      if (typeof reader.result === "string") {
+        setContent(reader.result);
+      }
+    };
+    reader.readAsText(event.target.files[0]);
+  }
   return (
-    <div style={{ width: "300px", height: "300px" }}>
-      <Dragger {...props} style={{ width: "300px", height: "300px" }}>
-        <p className="ant-upload-drag-icon">
-          <InboxOutlined />
-        </p>
-        <p className="ant-upload-text">
-          Clica o arrastra hasta aquí el fichero a subir
-        </p>
-        <p className="ant-upload-hint">Sólo soporta ficheros en formato csv</p>
-      </Dragger>
+    <div>
+      <input type="file" accept=".csv" onChange={onFileChange} />
+      <button>Upload!</button>
+      <Text>{upload?.name}</Text>
+      <Text>{content}</Text>
     </div>
   );
-};
-
-const props = {
-  accept: "text/csv",
-  name: "file",
-  multiple: false,
-  action: httpServices.uploadData,
-  onChange(info: { file: { name?: any; status?: any }; fileList: any }) {
-    const { status } = info.file;
-    if (status === "done") {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === "error") {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
 };
