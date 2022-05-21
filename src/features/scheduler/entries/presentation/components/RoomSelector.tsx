a9bd5f671e4201daf6094090c007aab8f44198f6
dolansete <dolancc00@gmail.com>
2022-05-21 19:38:18 +0200

    Selección de aulas

--- src/features/scheduler/entries/presentation/components/RoomSelector.tsx
+++ src/features/scheduler/entries/presentation/components/RoomSelector.tsx
@@ -1,6 +1,6 @@
 import { Form, Select } from "antd";
 import Text from "antd/lib/typography/Text";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Key } from "react";
 import { searchSlotsService } from "../../../../slots/domain/services/SearchSlots.service";
 import { SlotsFilterForm } from "../../../../slots/domain/models/SlotsFilterForm";
 import { Edifcio } from "../../../../slots/domain/models/Slots";
@@ -30,26 +30,55 @@ const Edificios: Edifcio[] = [
   },
 ];
 
-export const RoomSelector = () => {
+type Props = {
+  hour: string;
+};
+
+type AuxSlot = {
+  id: String;
+  name: String;
+};
+
+export const RoomSelector = ({ hour }: Props) => {
   const [edificio, setEdificio] = useState<Edifcio>(Edificios[0]);
-  const [floor, setFloor] = useState(Edificios[0].floors[0]);
-  const [roomList, setRoomList] = useState(["1", "2", "3"]);
+  const [floor, setFloor] = useState(Edificios[0].floors[2]);
+  const [roomList, setRoomList] = useState<AuxSlot[]>([{ id: "1", name: "1" }]);
 
   const menu = roomList.map((room, i) => (
-    <Option key={i} children={<Text>{room}</Text>} value={room} />
+    <Option
+      key={i}
+      children={<Text>{room.name}</Text>}
+      value={room.id as Key}
+    />
   ));
 
   const loadData = async () => {
     console.log(edificio, floor);
-    /*
     const params: SlotsFilterForm = {
-      day: formDate,
-      hour: formHour,
-      floor: values.floor,
-      capacity: values.capacity,
-      building: values.building,
+      //day: formDate ,
+      //hour:formHour,
+      floor: floor,
+      capacity: 0,
+      building: edificio.name,
     };
-    const slots = await searchSlotsService.filterBy();
+    const slots = await searchSlotsService.filterBy(params);
+    if (!slots.isError) {
+      var slotvalue = slots.value;
+      var auxslots: AuxSlot[] = [];
+      for (var i in slotvalue) {
+        if (slotvalue[i].kind == "AULA") {
+          var auxslot: AuxSlot = {
+            id: slotvalue[i].id,
+            name: slotvalue[i].name,
+          };
+          auxslots.push(auxslot);
+          console.log(auxslots);
+        }
+      }
+      setRoomList(auxslots);
+    }
+
+    /*
     if (slots.isError) message.error("Error al obtener las reservas");
     else {
       await delay(500);
@@ -64,6 +93,12 @@ export const RoomSelector = () => {
     loadData();
   }, []);
 
+  useEffect(() => {
+    //Runs on the first render
+    //And any time any dependency value changes
+    loadData();
+  }, [floor, edificio, hour]);
+
   return (
     <div>
       <Text>Edificio y planta: </Text>
@@ -75,7 +110,6 @@ export const RoomSelector = () => {
               return e.name == newEdificio;
             });
             setEdificio(newState[0]);
-            loadData();
           }}
         >
           {Edificios.map((edificio) => (
@@ -85,11 +119,10 @@ export const RoomSelector = () => {
           ))}
         </Select>
         <Select
-          defaultValue={edificio.floors[0]}
+          defaultValue={edificio.floors[1]}
           onChange={(newfloor) => {
             console.log(newfloor);
             setFloor(newfloor);
-            loadData();
           }}
         >
           {edificio.floors.map((floor) => (
